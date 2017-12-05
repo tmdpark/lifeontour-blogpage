ae18c5bb0186077dad0a8f53a6e4d93db7b997c8
Daniel Park <dpark@travismathew.com>
2017-12-04 16:49:01 -0800

    Webpack config && live reload.

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// app.js - Main entry point for application.
+// @author Daniel Park <dpark@travismathew.com>
+
+import './scss/app.scss';
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+
+const path = require('path'),
+      webpack = require('webpack'),
+      CleanWebpackPlugin = require('clean-webpack-plugin'),
+      HtmlWebpackPlugin = require('html-webpack-plugin'),
+      ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: './css/app.css'
+});
+
+const liveReloadOptions = {
+  protocol: 'https'
+}
+
+const config = {
+
+  // absolute path for project root with the 'src' folder
+  context: path.resolve(__dirname, 'src'),
+
+  entry: {
+    // relative path declaration
+    app: './app.js'
+  },
+
+  output: {
+    // absolute path declaration
+    path: path.resolve(__dirname, 'dist'),
+    filename: './js/[name].bundle.js'
+  },
+
+  module: {
+    rules: [
+
+      // babel-loader with 'env' preset
+      { test: /\.js$/, include: /src/, exclude: /node_modules/, use: { loader: "babel-loader", options: { presets: ['env'] } } },
+      // html-loader
+      { test: /\.html$/, use: ['html-loader'] },
+      // sass-loader with sourceMap activated
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src', 'scss')],
+        use: ['css-hot-loader'].concat(extractPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+          fallback: 'style-loader'
+        }))
+      },
+      // file-loader(for images)
+      { test: /\.(jpg|png|gif|svg)$/, use: [ { loader: 'file-loader', options: { name: '[name].[ext]', outputPath: './media/' } } ] },
+      // file-loader(for fonts)
+      { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] }
+
+    ]
+  },
+
+  plugins: [
+    // cleaning up only 'dist' folder
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    // extract-text-webpack-plugin instance
+    extractPlugin
+  ],
+
+  devServer: {
+    // static files served from here
+    contentBase: './src',
+    watchContentBase: true,
+    compress: true,
+    // open app in localhost:2000
+    port: 1234,
+    stats: 'errors-only',
+  },
+
+  devtool: 'inline-source-map'
+
+};
+
+module.exports = config;
